2c8170a6a1f273031112352ca7d1beffea8cb0bf
Jack Challis <jackechallis@hotmail.co.uk>
2024-08-17 11:46:54 +0100

    Initial commit
    commit from home computer

---
+++ plugin/source/PluginProcessor.cpp
@@ -0,0 +1,241 @@
+#include "DistortionVST/PluginProcessor.h"
+#include "DistortionVST/PluginEditor.h"
+
+//==============================================================================
+AudioPluginAudioProcessor::AudioPluginAudioProcessor()
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+{
+    state = std::make_unique<juce::AudioProcessorValueTreeState>(*this,nullptr);
+    state->createAndAddParameter("drive", "Drive", "Drive", juce::NormalisableRange<float>(0.f, 100.f, 0.0001f), 1.f, nullptr, nullptr);
+    state->createAndAddParameter("blend", "Blend", "Blend", juce::NormalisableRange<float>(0.f, 1.f, 0.0001f), 1.f, nullptr, nullptr);
+    state->createAndAddParameter("range", "Range", "Range", juce::NormalisableRange<float>(0.f, 1.f, 0.0001f), 1.f, nullptr, nullptr);
+    state->createAndAddParameter("volume", "Volume", "Volume", juce::NormalisableRange<float>(0.f, 1.f, 0.0001f), 1.f, nullptr, nullptr);
+
+    state->state = juce::ValueTree("drive");
+    state->state = juce::ValueTree("range");
+    state->state = juce::ValueTree("blend");    
+    state->state = juce::ValueTree("volume");
+}
+
+AudioPluginAudioProcessor::~AudioPluginAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String AudioPluginAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool AudioPluginAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool AudioPluginAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool AudioPluginAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double AudioPluginAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int AudioPluginAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int AudioPluginAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void AudioPluginAudioProcessor::setCurrentProgram (int index)
+{
+    juce::ignoreUnused (index);
+}
+
+const juce::String AudioPluginAudioProcessor::getProgramName (int index)
+{
+    juce::ignoreUnused (index);
+    return {};
+}
+
+void AudioPluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+    juce::ignoreUnused (index, newName);
+}
+
+//==============================================================================
+void AudioPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    juce::ignoreUnused (sampleRate, samplesPerBlock);
+}
+
+void AudioPluginAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+bool AudioPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+
+void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
+                                              juce::MidiBuffer& midiMessages)
+{
+    juce::ignoreUnused (midiMessages);
+
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+
+    float drive = *state->getRawParameterValue("drive");
+    float range = *state->getRawParameterValue("range");
+    float blend = *state->getRawParameterValue("blend");
+    float volume = *state->getRawParameterValue("volume");
+    
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        float* channelData = buffer.getWritePointer (channel);
+
+        for (int sample = 0; sample < buffer.getNumSamples(); sample++){
+
+            float cleanSig = *channelData;
+
+            *channelData *= drive * range;
+
+            //HARDCLIPPER
+            // if (*channelData > 1){
+            //    *channelData = 1;
+            // }
+            // else if (*channelData < -1){
+            //    *channelData = -1;
+            // }
+            // *channelData = ((*channelData * blend) + (cleanSig * (1.f - blend))) * volume;
+
+            //SOFTCLIPPER
+            *channelData = (((2.f / juce::float_Pi) * atan(*channelData) * blend) + (cleanSig * (1.f - blend))) * volume;
+
+            // //(distorted * blend) + (clean * (1-blend))
+
+            channelData++;
+
+        }
+
+    }
+}
+
+juce::AudioProcessorValueTreeState& AudioPluginAudioProcessor::getState() {
+    return *state;
+}
+
+//==============================================================================
+bool AudioPluginAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
+{
+    return new AudioPluginAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void AudioPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+    
+    juce::MemoryOutputStream stream(destData, false);
+    state->state.writeToStream(stream);
+
+}
+
+void AudioPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+
+    juce::ValueTree tree = juce::ValueTree::readFromData(data,sizeInBytes);
+
+    if (tree.isValid()){
+
+        state->state = tree;
+
+    } 
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new AudioPluginAudioProcessor();
+}
---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <DistortionVST/PluginProcessor.h>
+
+namespace DisortionVST_test{
+    TEST(AudioPluginAudioProcessor, Foo){
+        AudioPluginAudioProcessor processor{};
+        ASSERT_FALSE(true);
+    }
+}
