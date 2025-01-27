d02c351da09fad9a5c82696b2679e26373c79067
Jack Challis <jackechallis@hotmail.co.uk>
2025-01-27 13:35:23 +0000

    Remove ignored files from tracking

--- test/source/AudioProcessorTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <DistortionVST/PluginProcessor.h>
-
-namespace DisortionVST_test{
-    TEST(AudioPluginAudioProcessor, Foo){
-        AudioPluginAudioProcessor processor{};
-        ASSERT_FALSE(true);
-    }
-}
